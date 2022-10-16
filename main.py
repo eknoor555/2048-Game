3ebb749e1e6b96d32c2d2c4265f81e349f717516
eknoor555 <83641013+eknoor555@users.noreply.github.com>
2022-10-16 20:01:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 from tkinter import Frame,Label,CENTER
 import LogicinGame
 import Constants as c
-#def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    #print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     class Game2048(Frame):
         def __init__(self):
@@ -74,4 +63,3 @@ if __name__ == '__main__':
                         self.grid_cells[1][2].configure(text="LOSE!", bg=c.BACKGROUND_COLOR_CELL_EMPTY)
 
     gamegrid=Game2048()
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
